1a52319d10824e3339a216205c92c677d1e65864
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-10-15 12:25:36 +0530

    added localStorage support in theme switcher

--- Basic HTML,CSS & JavaScript/Dark-Light Mode Toggle/KaziNizamul/themeSwitcher.js
+++ Basic HTML,CSS & JavaScript/Dark-Light Mode Toggle/KaziNizamul/themeSwitcher.js
@@ -1,5 +1,28 @@
-const chk = document.getElementById('chk');
+window.onload = setTheme();
 
-chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark');
-});
+function setTheme() { 
+  const chk = document.getElementById('chk');
+
+  // initial load of theme , setting it as white 
+  if (localStorage.length === 0) { 
+    localStorage.setItem('theme', 'white');
+  }
+  // 
+  else {
+    if (localStorage.getItem('theme') === 'dark') {
+      document.body.classList.toggle('dark');
+    }
+  }
+
+
+  chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+
+    if (localStorage.getItem('theme') === 'white') localStorage.setItem('theme', 'dark');
+    else localStorage.setItem('theme', 'white');
+
+  });
+
+
+}
+  
